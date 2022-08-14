78299e95359505968271af1af1e17342228185c8
mizeo <michal.mros@gmail.com>
2022-08-14 22:11:28 +0200

    useEffect2

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,30 @@ import { useState } from 'react';
 
 function App() {
 
+  const[state, setState] = useState<Array<string>>([
+    // "Wielkoduszna Józefa z Trzebini",
+    // "Gustaw, Jasnowidz Wieży",
+    // "The Admin of Aerodrome",
+    // "Poor Thyri of the Chamber",
+    // "Mentor Arjan Blindlip",
+    // "She who Ostensibly Heard About Humanity",
+    // "Mustafuel, Spawn of Ghinismell",
+    // "Holdsworth, the Rogue of The Stars",
+    // "The Padwar that Makes Flight",
+    // "The Flowing Teacher"
+  ])
+
   let deckNames: Array<string>= [];
+
   useEffect(() => {
     (async () => {
       const response = await fetch("https://nodeforge.herokuapp.com/");
       const json = await response.text();
       const obj = JSON.parse(json);
       
+      // deckNames = [];
       for (let i=0; i<10; i++) {
+        
         let pos = i.toString();
         deckNames.push(obj["data"][pos]["name"]);
       }
@@ -25,34 +41,6 @@ function App() {
     
     console.log(deckNames);
   },[]);
-
-  const[state, setState] = useState<Array<string>>([
-  "Wielkoduszna Józefa z Trzebini",
-  "Gustaw, Jasnowidz Wieży",
-  "The Admin of Aerodrome",
-  "Poor Thyri of the Chamber",
-  "Mentor Arjan Blindlip",
-  "She who Ostensibly Heard About Humanity",
-  "Mustafuel, Spawn of Ghinismell",
-  "Holdsworth, the Rogue of The Stars",
-  "The Padwar that Makes Flight",
-  "The Flowing Teacher"
-])
-
-  async function Test() {
-    let response = await fetch("https://nodeforge.herokuapp.com/");
-    const json = await response.text();
-    const obj = JSON.parse(json);
-    
-    for (let i=0; i<10; i++) {
-      let pos = i.toString();
-      deckNames.push(obj["data"][pos]["name"]);
-    }
-  }
-    
-
-Test();
-
 
 function renderNames(array: Array<string>) {
   let names = [];
